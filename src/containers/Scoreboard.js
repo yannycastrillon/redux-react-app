8b6211e87fdb737aafa2cf9fbd4b1dd6d0151c82
Yanny Castrillon <yannyandres117@gmail.com>
2017-05-12 00:54:37 -0700

    Add action creator for add player, remove player and update player. Create the reducer player to be able to change the STORE state. Change the initial state from the Scoreboard container to the player's reducer.

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -9,18 +9,7 @@ import AddPlayerForm from '../components/AddPlayerForm'
 export default class Scoreboard extends Component {
   state = {
     players: [
-      {
-        name: 'Jim Hoskins',
-        score: 31,
-      },
-      {
-        name: 'Andrew Chalkley',
-        score: 20,
-      },
-      {
-        name: 'Alena Holligan',
-        score: 50,
-      }
+      
     ]
   };
 
